7fbcb8858a89d545cd3265e6965d50f469d11e43
Ania <Ania>
2017-05-03 23:12:49 +0200

    README renamed, space in line 8 of .js added

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var animal = 'Zielone słonie';
+var animal = 'Zielone słonie'.toUpperCase();
 
-var animalUpperCased = animal.toUpperCase();
+var animalCharsAfter = text.replace('Papugi', animal);
 
-var animalCharsAfter = text.replace('Papugi', animalUpperCased);
-
-var finalCharsAfter = animalCharsAfter.substr(0, animalCharsAfter.length/2)
+var finalCharsAfter = animalCharsAfter.substr(0, animalCharsAfter.length / 2);
 
 console.log(finalCharsAfter);
